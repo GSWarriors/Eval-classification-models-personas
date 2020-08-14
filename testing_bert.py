f98d24f02dfbd5f307fa0ffe0888400998cb3dd5
Siddartha Punj <sid.punj@gmail.com>
2020-08-14 13:35:10 -0700

    Added BERT model for predicting sentence relations

--- testing_bert.py
+++ testing_bert.py
@@ -13,15 +13,26 @@ def main():
     my_list = []
     dataframe = pd.read_csv("/Users/arvindpunj/Desktop/Projects/NLP lab research/Extracting-personas-for-text-generation/train_self_original.txt",
     delimiter='\n', header= None, error_bad_lines=False)
-    #print(dataframe)
     first_convo = dataframe[0]
     convo_string = filter_conversation(first_convo)
+
     #my_list.append(convo_string)
     first_sentence, second_sentence = filter_by_sentence(convo_string)
     print("First sentence: " + first_sentence)
     print("Second sentence: " + second_sentence)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
+    encoding = tokenizer(first_sentence, second_sentence, return_tensors='pt')
 
+    loss, logits = model(**encoding, next_sentence_label=torch.LongTensor([1]))
 
+    if logits[0, 0] < logits[0, 1]:
+        print("same sentence")
+    else:
+        print("different sentence")
+
+    print(str(logits[0, 0]))
+    print(str(logits[0, 1]))
     #tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     #model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
     #encoding = tokenizer()
@@ -32,8 +43,8 @@ def filter_conversation(first_convo):
     #print(first_convo)
 
     convo_string = ""
-    for line in range(5, 11):
-        #print(first_convo[line])
+    for line in range(8, 11):
+        print(first_convo[line])
         current_convo = first_convo[line]
         convo_without_number = current_convo[2:]
         #print(convo_without_number)
@@ -57,13 +68,10 @@ def filter_by_sentence(convo_string):
     second_sentence = ""
     first_end_index = 0
     tab_count = 0
-    print("convo string: " + convo_string)
 
     for char in range(0, len(convo_string)):
 
         if convo_string[char] == '\t' and tab_count < 2:
-            print("this is a tab")
-
             if tab_count == 0:
                 first_sentence += convo_string[0:char]
                 first_end_index = char
