1215d035c6856e18447a97b6a0f90c6ff269f402
Siddartha Punj <sid.punj@gmail.com>
2020-08-14 10:01:14 -0700

    Adding dataset set from personachat and bert testing file

---
+++ testing_bert.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def main():
+
+    #feed in data through pandas
+    #with open('ParlAI/data/Persona-Chat/personachat/test_none_original.txt', 'r') as f:
+    #    contents =
+    dataframe = pd.read_csv("ParlAI/data/Persona-Chat/personachat/train_self_original.txt",
+    delimiter='\n', header= None, error_bad_lines=False)
+    #print(dataframe)
+    first_convo = dataframe[0]
+    #print("first line: " + dataframe[0][0:11])
+    filter_conversation(first_convo)
+
+
+
+
+def filter_conversation(first_convo):
+    #print(first_convo)
+    for line in range(5, 11):
+        print(first_convo[line])
+        #print()
+
+
+
+
+main()
