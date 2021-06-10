944b11eb0802e6fc92cb24c7fdff92d1cecaac15
Siddartha Punj <sid.punj@gmail.com>
2021-06-10 14:25:11 -0700

    Added tagging for persona and word freq dict for responses

--- code/persona_word_importance.py
+++ code/persona_word_importance.py
@@ -12,6 +12,7 @@ import itertools
 import json
 from matplotlib import pyplot
 import nltk
+import string
 
 from mymodel import DistilBertTrainingParams
 from mymodel import DistilBertandBilinear
@@ -66,9 +67,6 @@ def modify_responses(test_personas, encoded_test_dict, saved_model, training_par
             line = line.strip('\n')
             stopwords_list.append(line)
 
-    #print("stopwords list: " + str(stopwords_list))
-
-
 
 
     for i in range(0, len(test_personas)):
@@ -76,34 +74,97 @@ def modify_responses(test_personas, encoded_test_dict, saved_model, training_par
         response_convo = test_data[i]['text snippet']
         #persona_encoding = [training_params.tokenizer.encode(persona_convo, add_special_tokens=True)]
         #gold_snippet_encoding = encoded_test_dict[i]
-        print("persona before split: " + str(persona_convo))
-        print()
+        #print("persona before split: " + str(persona_convo))
+        #print()
 
 
-        persona_convo = persona_convo.split()
-        #persona_convo = nltk.word_tokenize(persona_convo)
-        print("persona after split: " + str(persona_convo))
+        filtered_words_list = tag_persona_and_create_dict(persona_convo, stopwords_list)
+        create_response_freq_dict(response_convo, filtered_words_list, stopwords_list)
 
-
-        freq_dict = {}
-
-        for j in range(0, len(persona_convo)):
-            curr_word = persona_convo[j]
-            if curr_word not in stopwords_list:
-                if curr_word not in freq_dict:
-                    freq_dict[curr_word] = 1
-                else:
-                    freq_dict[curr_word] += 1
-
-        print("frequency dict: " + str(freq_dict))
-        #print("response: " + str(response_convo))
-
+        #combine persona and response dicts
 
         if i == 0:
             break
 
 
 
+def tag_persona_and_create_dict(persona_convo, stopwords_list):
+
+    #tokenize persona into separate words
+    persona_convo = nltk.word_tokenize(persona_convo)
+    #print("persona after tokenization: " + str(persona_convo))
+    #print()
+
+    persona_dict = {}
+    final_dict = {}
+    persona_words_list = []
+
+    #remove stopwords, get the frequency of words from persona
+    for i in range(0, len(persona_convo)):
+        curr_elem = persona_convo[i]
+        if curr_elem not in stopwords_list and curr_elem not in string.punctuation:
+            persona_words_list.append(curr_elem)
+
+            if curr_elem not in persona_dict:
+                persona_dict[curr_elem] = 1
+            else:
+                persona_dict[curr_elem] += 1
+
+    #print("frequency dict: " + str(persona_dict))
+    print("persona after removing punctuation: " + str(persona_words_list))
+    print()
+    tagged = nltk.pos_tag(persona_words_list)
+
+    print("tagged list: " + str(tagged))
+    print()
+
+    noun_tags = ['NN', 'NNS', 'NNP', 'NNPS']
+
+    filtered_words_list = list(filter(lambda x: x[1] in noun_tags, tagged))
+
+    #record the frequency of the response dict with only nouns in the persona
+    print("filtered persona list: " + str(filtered_words_list))
+    print()
+    return filtered_words_list
+
+
+
+
+def create_response_freq_dict(response_convo, filtered_words_list, stopwords_list):
+
+    #tokenize response
+
+    response_dict = {}
+    for i in range(0, len(response_convo)):
+        response_convo[i] = nltk.word_tokenize(response_convo[i])
+        curr_response = response_convo[i]
+
+        #check for stopwords and punctuation. then, add to dict
+        for j in range(0, len(curr_response)):
+            if (curr_response[j] not in stopwords_list) and curr_response[j] not in string.punctuation:
+                if curr_response[j] not in response_dict:
+                    response_dict[curr_response[j]] = 1
+                else:
+                    response_dict[curr_response[j]] += 1
+
+    print("the response dict: " + str(response_dict))
+
+
+
+
+#code to combine persona and response- should be in prev function (modify responses)
+
+"""print("persona frequency dict: " + str(persona_dict))
+    print()
+    print("response frequency dict: " + str(response_dict))
+    print()
+
+    #add values from both persona and response into a final dict
+    for key, val in response_dict.items():
+        if key not in final_dict:
+            final_dict[key] = persona_dict[key] + val
+
+    print("the final dict: " + str(final_dict))"""
 
 
 
